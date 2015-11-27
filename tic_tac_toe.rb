1283980843675f623f5cc2126a6571ae65a258bf
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-11-26 18:50:15 -0600

    different road

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,3 @@
-class Player
-  def initialize(name, marker)
-    @name = name
-    @marker = marker
-  end
-
-  def take_turn
-    puts "Where do you want to mark?\n"
-    requested_move = gets.chomp.to_sym
-  end
-end
-
 class Game
   def initialize
     @position_values = {:a1 => " ", :b1 => " ", :c1 => " ",
@@ -21,24 +9,28 @@ class Game
   #{@position_values[:a2]} | #{@position_values[:b2]} | #{@position_values[:c2]}\n
   -- --- --\n
   #{@position_values[:a3]} | #{@position_values[:b3]} | #{@position_values[:c3]}\n"
+  
+  	# 8 possibilities
+  	@winning_boards = [[:a1, :b1, :c1], # horizontal
+                       [:a2, :b2, :c2],
+                       [:a3, :b3, :c3],
+
+                       [:a1, :a2, :a3], # vertical
+                       [:b1, :b2, :b3],
+                       [:c1, :c2, :c3],
+
+                       [:a1, :b2, :c3], # diagonal
+                       [:a3, :b2, :c1]]
+
   end
 
   def print_board
     puts @board
   end
-
-  def win_condition?
-    # 8 possibilities
-    winning_boards = [[:a1, :b1, :c1], # horizontal
-                      [:a2, :b2, :c2],
-                      [:a3, :b3, :c3],
-
-                      [:a1, :a2, :a3], # vertical
-                      [:b1, :b2, :b3],
-                      [:c1, :c2, :c3],
-
-                      [:a1, :b2, :c3], # diagonal
-                      [:a3, :b2, :c1]]
+  
+  def take_turn
+    puts "Where do you want to mark?\n"
+    requested_move = gets.chomp.to_sym
   end
 
   def position_occupied?(symbol)
@@ -50,12 +42,13 @@ class Game
   end
 end
 
-player1 = Player.new("Diarmuid", "X")
-player2 = Player.new("Christina", "O")
+class Player
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
+  end
+end
 
 our_game = Game.new
-our_game.print_board
 
-# player1.take_turn
-
-puts our_game.position_occupied?(a1)
+player1 = Player.new("Diarmuid", "X")
