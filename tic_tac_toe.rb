7d745403da3e95e964784787d606fce1ae1a1b50
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-11-26 20:06:39 -0600

    fixed broken board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,12 +3,7 @@ class Game
     @position_values = {:a1 => " ", :b1 => " ", :c1 => " ",
                         :a2 => " ", :b2 => " ", :c2 => " ",
                         :a3 => " ", :b3 => " ", :c3 => " "}
-    @board = "
-  #{@position_values[:a1]} | #{@position_values[:b1]} | #{@position_values[:c1]}\n
-  -- --- --\n
-  #{@position_values[:a2]} | #{@position_values[:b2]} | #{@position_values[:c2]}\n
-  -- --- --\n
-  #{@position_values[:a3]} | #{@position_values[:b3]} | #{@position_values[:c3]}\n"
+		# @board = 
   
   	# 8 possibilities
   	@winning_boards = [[:a1, :b1, :c1], # horizontal
@@ -21,16 +16,26 @@ class Game
 
                        [:a1, :b2, :c3], # diagonal
                        [:a3, :b2, :c1]]
-
+    @turns_taken = 0
   end
-
+  
   def print_board
-    puts @board
+		puts "
+  #{@position_values[:a1]} | #{@position_values[:b1]} | #{@position_values[:c1]}
+  --+---+--
+  #{@position_values[:a2]} | #{@position_values[:b2]} | #{@position_values[:c2]}
+  --+---+--
+  #{@position_values[:a3]} | #{@position_values[:b3]} | #{@position_values[:c3]}\n\n"
   end
   
   def take_turn
+  	print_board
     puts "Where do you want to mark?\n"
     requested_move = gets.chomp.to_sym
+		@position_values[requested_move] = "X"
+		@turns_taken += 1
+		puts @position_values[:a1]
+		print_board
   end
 
   def position_occupied?(symbol)
@@ -51,4 +56,4 @@ end
 
 our_game = Game.new
 
-player1 = Player.new("Diarmuid", "X")
+our_game.take_turn
